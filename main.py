f1336c4bc0956a568695ea66f50a4069b339863c
Energetic3906 <101653181+Energetic3906@users.noreply.github.com>
2024-07-05 13:30:27 +0800

    Delete duplicate code

--- main.py
+++ main.py
@@ -601,7 +601,6 @@ async def subscribe(event):
   splitd = [i for i in regex.split(r'\s+', text) if i]  # 删除空元素
   if len(splitd) <= 1:
     await event.respond(r'输入需要订阅的关键字,支持js正则语法：`/[\s\S]*/ig`\n\nInput the keyword that needs to subscribe, support JS regular syntax：`/[\s\S]*/ig`')
-    await event.respond(r'输入需要订阅的关键字,支持js正则语法：`/[\s\S]*/ig`\n\nInput the keyword that needs to subscribe, support JS regular syntax：`/[\s\S]*/ig`')
     cache.set('status_{}'.format(chat_id),{'current_status':'/subscribe keywords','record_value':text},expire=5*60)#设置5m后过期
   elif len(splitd)  == 3:
     command, keywords, channels = splitd
